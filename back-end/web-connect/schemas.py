b3e4065f0541a5c4fb68a3c4b8223fa7ab107c40
EdgarV7 <73083234+EdgarV7@users.noreply.github.com>
2022-10-26 10:29:19 -0500

    mi-primer-commit

---
+++ back-end/web-connect/schemas.py
@@ -0,0 +1,54 @@
+from typing import List, Union
+
+from pydantic import BaseModel
+
+
+class ItemBase(BaseModel):
+    title: str
+    description: Union[str, None] = None
+
+
+class ItemCreate(ItemBase):
+    pass
+
+
+class Item(ItemBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    items: List[Item] = []
+
+    class Config:
+        orm_mode = True
+
+class UserInDB(User):
+    hashed_password: str
+
+class TokenData(BaseModel):
+    email: Union[str, None] = None
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class User(BaseModel):
+    username: str
+    email: Union[str, None] = None
+    full_name: Union[str, None] = None
+    disabled: Union[bool, None] = None
+
